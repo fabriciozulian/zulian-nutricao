d425b961d85ecec8f772d973a6b2ca50297e5322
Fabricio Zulian <fabriciozulian@outlook.com>
2025-07-28 21:02:40 -0300

    feat: incluí método de adicionar novo paciente

--- js/script.js
+++ js/script.js
@@ -1,27 +1,63 @@
+const frm = document.querySelector("form");
+const tabela = document.querySelector("#tabela-pacientes");
 
-let pacientes = document.querySelectorAll(".paciente");
-for(paciente of pacientes){
-    calculaImc(paciente);
+
+atualizarDados();
+
+function atualizarDados(){
+    let pacientes = document.querySelectorAll(".paciente");
+    for (paciente of pacientes) {
+        calculaImc(paciente);
+    }
 }
 
-function calculaImc(paciente){
-    const peso = (paciente.querySelector(".info-peso")).textContent;
-    const altura = (paciente.querySelector(".info-altura")).textContent;
+function calculaImc(paciente) {
+    const peso = Number((paciente.querySelector(".info-peso")).textContent);
+    const altura = Number((paciente.querySelector(".info-altura")).textContent);
     const imc = paciente.querySelector(".info-imc");
     let alturaStatus = true;
     let pesoStatus = true
-    
-    if(peso <= 0 || peso >=1000){
+
+    if (peso <= 0 || peso >= 1000) {
         imc.textContent = "Peso inválido";
         pesoStatus = false;
+        imc.style.background = "red";
     }
 
-    if(altura <=0 || altura >= 3.00){
+    if (altura <= 0 || altura >= 3.00) {
         imc.textContent = "Altura é inválida";
         alturaStatus = false;
     }
-    
-    if(alturaStatus && pesoStatus){
-        imc.textContent = (peso/(altura*altura)).toFixed(2);
+
+    if (alturaStatus && pesoStatus) {
+        imc.textContent = (peso / (altura * altura)).toFixed(2);
     }
 }
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const tr = document.createElement("tr");
+    tr.classList.add("paciente");
+    const nomeTd = document.createElement("td");
+    nomeTd.classList.add("info-nome");
+    nomeTd.textContent = frm.inNome.value;
+    const pesoTd = document.createElement("td");
+    pesoTd.classList.add("info-peso");
+    pesoTd.textContent = frm.inPeso.value;
+    const alturaTd = document.createElement("td");
+    alturaTd.classList.add("info-altura");
+    alturaTd.textContent = frm.inAltura.value;
+    const gorduraTd = document.createElement("td");
+    gorduraTd.classList.add("info-gordura");
+    gorduraTd.textContent = frm.inGordura.value;
+    const imcTd = document.createElement("td");
+    imcTd.classList.add("info-imc");
+    imcTd.textContent = 0;
+    tr.appendChild(nomeTd);
+    tr.appendChild(pesoTd);
+    tr.appendChild(alturaTd);
+    tr.appendChild(gorduraTd);
+    tr.appendChild(imcTd);
+    tabela.appendChild(tr);
+    atualizarDados();
+})
