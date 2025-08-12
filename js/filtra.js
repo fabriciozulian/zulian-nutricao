ee1f43e303b0fdf31d74a8924f55ae2e2435220c
Fabricio de Oliveira Zulian <fabricio.zulian@microwork.inf.br>
2025-08-12 17:14:03 -0300

    feat: adiciona função de filtrar pacientes

--- js/filtra.js
+++ js/filtra.js
@@ -1,5 +1,22 @@
 const inputFiltra = document.querySelector("#filtrar-tabela");
 
 inputFiltra.addEventListener("input", (x) => {
-    console.log(x.target.value);
-})
+    const pacientes = document.querySelectorAll(".paciente");
+    //console.log(x.target.value);
+    const expressao = new RegExp(x.target.value,"i");
+    if(x.target.value.length > 0){
+        for (paciente of pacientes){
+            if(expressao.test(paciente.querySelector(".info-nome").textContent)){
+                console.log(paciente.querySelector(".info-nome").textContent);
+                console.log(paciente);
+                paciente.classList.remove("invisivel");
+            }else {
+                paciente.classList.add("invisivel");
+            }
+        }
+    } else {
+        for (paciente of pacientes){
+            paciente.classList.remove("invisivel");
+        }
+    }
+    })
